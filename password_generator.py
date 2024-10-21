4104bf4d76bcaab2a53d0a70a7d72c9f853a43e5
stefanrogic <stefanrogic@protonmail.com>
2024-10-21 15:54:28 +0200

    added loops for password generation

--- password_generator.py
+++ password_generator.py
@@ -5,6 +5,28 @@ numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
 print("Welcome to the PyPassword Generator!")
-nr_letters= int(input("How many letters would you like in your password?\n"))
-nr_symbols = int(input(f"How many symbols would you like?\n"))
-nr_numbers = int(input(f"How many numbers would you like?\n"))
+
+password = ""
+password_list = []
+
+nr_letters= int(input("How many letters would you like in your password?: "))
+nr_numbers = int(input(f"How many numbers would you like?: "))
+nr_symbols = int(input(f"How many symbols would you like?: "))
+
+for char in range(1, nr_letters + 1):
+    password_list.append(random.choice(letters))
+
+for char in range(1, nr_numbers + 1):
+    password_list.append(random.choice(numbers))
+
+for char in range(1, nr_symbols + 1):
+    password_list.append(random.choice(symbols))
+
+
+
+random.shuffle(password_list)
+
+for char in password_list:
+    password += char
+
+print(f"Your password is: {password}")
